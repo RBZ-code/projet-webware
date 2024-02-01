91e3b78e2fbb7a6263ede0235cbd5938074d7456
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-02-01 15:05:48 +0100

    Added table to order list

--- src/views/BackOrders.vue
+++ src/views/BackOrders.vue
@@ -1,31 +1,44 @@
 <template>
-    <nav class="back-nav">
-        <router-link to="/back-products">Produits</router-link>
-        <router-link to="/back-categories">Catégories</router-link>
-        <router-link to="/back-users">Utilisateurs</router-link>
-        <router-link to="/back-orders">Commandes</router-link>
-    </nav>
+    <BackNav />
     <div class="listing-products">
-        <div class="listing-box" v-for="(prod, index) in products" :key="index">
-            <figure>
-                <img :src="prod.image" :alt="prod.titre" />
-            </figure>
-            <div>
-                <h2>{{ prod.titre }}</h2>
-                <p>{{ prod.description }}</p>
-                <p>{{ prod.prix + "€" + " - MOQ " + prod.moq }}</p>
-            </div>
-        </div>
+        <div class="listing-template">
+        <table class="listing-tab">
+            <thead>
+                <tr>
+                    <th>N° commande</th>
+                    <th>Montant</th>
+                    <th>Utilisateur</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(order, index) in orders" :key="index">
+                    <td>{{ order.id }}</td>
+                    <td>{{ order.coutTotal + "€" }}</td>
+                    <td>{{ order.userId }}</td>
+                    <td>
+                        <MyButton label="Modifier" modifier="edit" @GeneralEventBtn="changeOrderStatus()" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     </div>
 </template>
 
 <script>
+import BackNav from "@/components/FrontOffice/BackNav.vue";
+import MyButton from "@/components/FrontOffice/MyButton.vue";
+
 export default {
     components: {
-
+        BackNav,
+        MyButton
     },
     computed: {
-
+        orders() {
+            return this.$store.state.commandes;
+        },
     },
 };
 </script>
